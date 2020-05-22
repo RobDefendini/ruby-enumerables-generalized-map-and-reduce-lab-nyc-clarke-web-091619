8111eefe51db3af638d4fda5296aff771377a133
Robert Defendini <robdefendini@gmail.com>
2020-05-22 20:31:55 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,14 @@
-# Your Code Here
+######## my-code 
+def map(array, &eq)
+  array.reduce([]) do
+     |a, i| a.push(eq[i]) 
+  end
+end
+######## reduce
+def reduce(a, s = 0, &block) 
+   new_array = array.reduce(s)  do
+     |a, i| a + i      if true 
+       return true 
+      p  new_array
+   end
+end
